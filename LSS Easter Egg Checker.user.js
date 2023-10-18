e1f52c4e8e42572292a79dbe866d6812c4cd4d99
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-10-18 21:40:25 +0200

    Update LSS Easter Egg Checker.user.js
    +added Sounds for Easter and Chrismas
    +added Speed Variation

--- LSS Easter Egg Checker.user.js
+++ LSS Easter Egg Checker.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         LSS Easter Egg Checker
 // @namespace    www.leitstellenspiel.de
-// @version      1.0
-// @description  Prüfe ob Osterei, wenn ja Ton und Ausrufezeichen
+// @version      1.1
+// @description  Prüfe auf Ostereier und spiele entsprechende Sounds ab
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/missions/*
 // @grant        none
@@ -14,33 +14,49 @@
     // Prüfe, ob das Element mit der ID "easter-egg-link" vorhanden ist
     var easterEggLink = document.getElementById('easter-egg-link');
 
-if (easterEggLink) {
-    // Lade den vorhandenen Sound auf der Website
-    var audio = new Audio('https://github.com/Sobol0202/LSS-EasterEggHunter/raw/main/decidemp3-14575.mp3');
+    if (easterEggLink) {
+        var imageSrc = easterEggLink.querySelector('img').getAttribute('src');
+        var audioSrc = '';
 
-    // Warte auf das 'canplaythrough'-Event, um sicherzustellen, dass der Sound vollständig geladen ist
-    audio.addEventListener('canplaythrough', function() {
+        // Prüfe, ob der Bild-Quelltext das Wort "Pumpkin" enthält
+        if (imageSrc.includes('pumpkin')) {
+            audioSrc = 'https://github.com/Sobol0202/LSS-EasterEggHunter/raw/main/boo-and-laugh-7060.mp3'; // URL zum Sound für Pumpkin
+        } else if (imageSrc.includes('Easter')) {
+            audioSrc = 'https://github.com/Sobol0202/LSS-EasterEggHunter/raw/main/jump-15984.mp3'; // URL zum Sound für Easter
+        } else if (imageSrc.includes('Christmas')) {
+            audioSrc = 'https://github.com/Sobol0202/LSS-EasterEggHunter/raw/main/hohoho-36506.mp3'; // URL zum Sound für Christmas
+        }
 
-        // Spiele den Sound ab
-        audio.play();
+        if (audioSrc) {
+            var audio = new Audio(audioSrc);
 
-        // Erstelle ein großes rotes Ausrufezeichen und füge es zum Body hinzu
-        var exclamationMark = document.createElement('div');
-        exclamationMark.innerHTML = '<span style="font-size: 1000px; color: red;">!!!</span>'; // Passen Sie die Schriftgröße an, um das Ausrufezeichen zu vergrößern
-        exclamationMark.style.position = 'fixed';
-        exclamationMark.style.top = '0';
-        exclamationMark.style.left = '0';
-        exclamationMark.style.width = '100%';
-        exclamationMark.style.height = '100%';
-        exclamationMark.style.display = 'flex';
-        exclamationMark.style.justifyContent = 'center';
-        exclamationMark.style.alignItems = 'center';
-        document.body.appendChild(exclamationMark);
+            // Zufällige Geschwindigkeit zwischen 0,9 und 1,1 festlegen
+            audio.playbackRate = 0.5 + Math.random();
 
-        // Entferne das Ausrufezeichen nach 2 Sekunden
-        setTimeout(function() {
-            exclamationMark.remove();
-        }, 2000);
-    });
-}
+            // Warte auf das 'canplaythrough'-Event, um sicherzustellen, dass der Sound vollständig geladen ist
+            audio.addEventListener('canplaythrough', function() {
+
+                // Spiele den Sound ab
+                audio.play();
+
+                // Erstelle ein großes rotes Ausrufezeichen und füge es zum Body hinzu
+                var exclamationMark = document.createElement('div');
+                exclamationMark.innerHTML = '<span style="font-size: 1000px; color: red;">!!!</span>';
+                exclamationMark.style.position = 'fixed';
+                exclamationMark.style.top = '0';
+                exclamationMark.style.left = '0';
+                exclamationMark.style.width = '100%';
+                exclamationMark.style.height = '100%';
+                exclamationMark.style.display = 'flex';
+                exclamationMark.style.justifyContent = 'center';
+                exclamationMark.style.alignItems = 'center';
+                document.body.appendChild(exclamationMark);
+
+                // Entferne das Ausrufezeichen nach 2 Sekunden
+                setTimeout(function() {
+                    exclamationMark.remove();
+                }, 2000);
+            });
+        }
+    }
 })();
