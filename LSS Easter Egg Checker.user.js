1816a2d60cc86c0b02edd6c7d378c5b26475388e
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2024-02-02 11:15:06 +0100

    Update LSS Easter Egg Checker.user.js
    +added valentinesday

--- LSS Easter Egg Checker.user.js
+++ LSS Easter Egg Checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Easter Egg Checker
 // @namespace    www.leitstellenspiel.de
-// @version      1.7
+// @version      1.8
 // @description  Prüfe auf Ostereier und zeige das entsprechende Symbol im Easter-Egg-Element kurz in Groß an
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/missions/*
@@ -29,6 +29,9 @@
         } else if (imageSrc.includes('Easter')) {
             audioSrc = 'https://github.com/Sobol0202/LSS-EasterEggHunter/raw/main/jump-15984.mp3'; // URL zum Sound für Easter
             symbol = '🐰';
+        } else if (imageSrc.includes('heart')) {
+            audioSrc = 'https://github.com/Sobol0202/LSS-EasterEggHunter/raw/main/kiss.mp3'; // URL zum Sound für Easter
+            symbol = '❤️';
         } else if (imageSrc.includes('santa')) {
             audioSrc = 'https://github.com/Sobol0202/LSS-EasterEggHunter/raw/main/hohoho-36506.mp3'; // URL zum Sound für Christmas
             symbol = '🎅';
